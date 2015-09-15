4894860572c723a3f7e29bf9ac4d08c7479ed386
Jacob Elder <jacob@boundless.com>
2015-09-15 18:23:27 -0400

    Fix config stuff

--- github.go
+++ github.go
@@ -5,18 +5,9 @@ package main
 import (
 	"fmt"
 	. "github.com/jelder/goenv"
+	"strings"
 )
 
-func init() {
-	if !GitHubIsConfigured() {
-		fmt.Println("GitHub is not configured.")
-	}
-}
-
-func GitHubIsConfigured() bool {
-	return ENV["GITHUB_USER"] != "" && ENV["GITHUB_TOKEN"] != ""
-}
-
 func GitHubRepo(app string) string {
-	return ENV[fmt.Sprint("GITHUB_REPO_", app)]
+	return ENV[fmt.Sprint("GITHUB_REPO_", strings.Replace(app, "-", "_", -1))]
 }
